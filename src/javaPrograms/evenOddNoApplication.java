59e9acb32e39f891d94aa3339b5e1dfac561aa72
GauriChavan9trix <gauri.chavan@navaantrix.com>
2026-07-18 17:41:13 +0530

    solve prime and even Number program

--- src/javaPrograms/evenOddNoApplication.java
+++ src/javaPrograms/evenOddNoApplication.java
@@ -1,10 +1,20 @@
 package javaPrograms;
 
+import java.util.Scanner;
+
 public class evenOddNoApplication {
 
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any number");
+		int n=sc.nextInt();
 		
+		if(n%2==0) {
+			System.out.println("Even Number");
+		}else {
+			System.out.println("Odd Number");
+		}
 
 	}
 
