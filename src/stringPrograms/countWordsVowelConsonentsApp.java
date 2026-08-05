24d16733ea360f9612334fd03545bec7b036eadb
Gauri-8940 <mehhgauri7@gmail.com>
2026-08-05 16:15:37 +0530

    solve array dsa problems

--- src/stringPrograms/countWordsVowelConsonentsApp.java
+++ src/stringPrograms/countWordsVowelConsonentsApp.java
@@ -5,26 +5,38 @@ import java.util.Scanner;
 class countWordsVowelConsonentsApp {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter any string");
-		String str = s.nextLine();
-		int v = 0, w = 1, c = 0;
 
-		
-		for (int i = 0; i < str.length();i++) {
-			char ch=str.charAt(i);
-			if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A'
-					|| ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') {
-				v++;
-			} else if (ch[i] == ' ' && ch[i + 1] != ' ') {
-				w++;
-			} else {
-				c++;
-			}
-		}
-		System.out.println("vowel " + v);
-		System.out.println("consonet " + c);
-		System.out.println("total words " + w);
-	}
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter any string");
+        String str = s.nextLine();
+
+        int v = 0, c = 0, w = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                    || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+                v++;
+
+            } else if (ch == ' ') {
+
+                if (i < str.length() - 1 && str.charAt(i + 1) != ' ') {
+                    w++;
+                }
+
+            } else {
+
+                c++;
+            }
+        }
+
+        System.out.println("Vowels = " + v);
+        System.out.println("Consonants = " + c);
+        System.out.println("Words = " + w);
+    }
 
 }
